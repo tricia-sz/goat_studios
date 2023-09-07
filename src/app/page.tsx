46c53dd0f2995427ae23910aeef3df24057be03b
Patrícia Souza <patricia.sz.br@gmail.com>
2023-09-06 22:49:34 -0300

    Final design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,16 +37,15 @@ export default async function Home() {
   return (
     <main className='w-full'>
       <Container>
-        <h1 className='text-left font-bold text-3xl mt-20 mb-10 text-sky-100'>
-          Games Recomendados
-        </h1>
+        <Input />
+        <h1 className='text-left font-bold text-3xl mt-20 mb-10 text-slate-400'></h1>
         <Link href={`/game/${cabritaStudio.id}`}>
-          <section className='w-full bg-slate-950 rounded-lgborder-indigo-500/100'>
+          <section className='w-full  rounded-lgborder-indigo-500/100'>
             <div className='w-full max-h-96 h-96 relative rounded-lg '>
               <div className='absolute justify-center items-center gap-2 z-20 bottom-0 p-4 '>
-                <p className='font-bold text-xl text-white flex gap-2'>
+                <p className='font-bold text-xl text-slate-300 flex gap-2'>
                   {cabritaStudio.title}
-                  <BsArrowRightCircle size={24} color='#ffffff' />
+                  <BsArrowRightCircle size={24} color='#e3f0ff' />
                 </p>
               </div>
               <Image
@@ -55,21 +54,22 @@ export default async function Home() {
                 priority={true}
                 quality={100}
                 fill={true}
-                className='max-h-96 object-cover rounded-lg opacity-50   hover:opacity-100 transition-all duration-300 box shadow-lg border-b border-sky-500 shadow-sky-600'
+                className='max-h-96 object-cover rounded-lg opacity-50   hover:opacity-100 transition-all duration-300 box shadow-lg shadow-sky-700/50 '
                 sizes='(max-width: 768px) 100vw, (max-width: 1200px) 44vw'
               />
             </div>
           </section>
         </Link>
 
-        <h2 className='text-left font-bold text-3xl mt-20 mb-20  text-sky-100'>
+        <h2 className='text-left font-bold text-4xl mt-20 mb-20  text-slate-400'>
           Os Mais Jogados da Semana
         </h2>
-        <section className='grid gap-7 ms:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <section className='bg-slate-950 grid gap-7 ms:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
           {data.map((item) => (
             <GameCard key={item.id} data={item} />
           ))}
         </section>
+        <h2 className='mt-40 mb-40'></h2>
       </Container>
     </main>
   );
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,9 +5,9 @@ import { AiFillGithub } from 'react-icons/ai';
 
 export function Footer() {
   return (
-    <footer className='w-full h-36  bg-slate-900 text-slate-300 shadow-lg shadow-blue-600'>
+    <footer className='w-full h-36  bg-slate-900 border-t border-sky-600 text-slate-500 shadow-lg shadow-blue-600'>
       <div className='max-w-screen-xl  pt-8 mx-auto flex  justify-center items-center  h-28 sm:justify-center'>
-        <AiFillGithub color='#ffffff' size={40} />
+        <AiFillGithub color='#838e9c' size={40} />
         <Link href='/'> By: Trícia Souza © 2023 </Link>
       </div>
     </footer>
