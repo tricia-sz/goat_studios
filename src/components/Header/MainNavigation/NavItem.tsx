9fbf41e80dc6bec5b47f246482c8b9958a5a77f4
Patrícia Souza <patricia.sz.br@gmail.com>
2023-09-19 09:44:33 -0300

    Size icons

--- src/components/Header/MainNavigation/NavItem.tsx
+++ src/components/Header/MainNavigation/NavItem.tsx
@@ -9,7 +9,7 @@ export interface NavItemProps {
 export function NavItem({title, icon: Icon}: NavItemProps){
   return(
     <a href="" className="w-full group flex items-center gap-3 roudend px-8 py-3 hover:bg-slate-950 rounded-full ">
-      <Icon className="h-5 w-5 text-sky-500 group-hover:text-sky-500 "/>
+      <Icon className="h-8 w-12 text-sky-500 group-hover:text-sky-500 "/>
       <span className="font-medium text-slate-300 group-hover:text-sky-500 w-full">
         {title}
       </span>
