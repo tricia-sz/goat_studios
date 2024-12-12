7b144c3cbde261f8e1a38edb610241d01037a65b
Patricia Souza <patricia.sz.br@gmail.com>
2024-12-12 08:07:19 -0300

    link git footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ export function Footer() {
       <footer className='w-full h-36 bg-slate-900  text-slate-500 shadow-lg shadow-blue-600'>
       <div className='max-w-screen-xl  pt-8 mx-auto flex  justify-center items-center  h-28 sm:justify-center'>
         <AiFillGithub color='#838e9c' size={40} />
-        <Link href='/'> By: Trícia Souza © 2023 </Link>
+        <Link href='https://github.com/tricia-sz' target='_blank'> By: Trícia Souza © 2023 </Link>
       </div>
     </footer>
     </div>
